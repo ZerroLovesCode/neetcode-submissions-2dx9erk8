8fb829a85ff58024cf0ac27c5e4e087efce12f76
Zee <88308160+ZerroLovesCode@users.noreply.github.com>
2026-04-30 17:32:41 +0530

    Add: three-integer-sum - submission-2

---
+++ Data Structures & Algorithms/three-integer-sum/submission-2.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        for(int i=0;i<n;i++){
+            int x = nums[i];
+            if(i>0 && nums[i-1] == nums[i]){
+                continue;
+            }
+            int target = -x;
+            // find all pairs y, z st y + z = target;
+            int l = i+1, r = n-1;
+            while(l < r){
+                int sum = nums[l] + nums[r];
+                if(sum == target){
+                    ans.push_back({x, nums[l], nums[r]});
+                    while(nums[l] == nums[l+1]){
+                        l++;
+                    }
+                    l++;
+                }
+                if(sum < target){
+                    l++;
+                }else{
+                    r--;
+                }
+            }
+        }
+        return ans;
+    }
+};
